cf7f4b48ea291ad78c94195730cfdc8d2d431a11
sutima <sutima_eci@kkumail.com>
2020-11-21 18:55:25 +0700

    employee

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import path from "path";
 
 import orderRouter from "./routers/orders.js";
+import employeeRouter from "./routers/employees.js";
 
 const port = 3000;
 const app = express();
@@ -10,6 +11,7 @@ const _dirname = path.resolve();
 
 app.use(bodyParser.json());
 app.use("/orders", orderRouter);
+app.use("/employees", employeeRouter)
 
 app.get("/", (req, res) => {
     res.json({ msg: "welcome delivery" });
