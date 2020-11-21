693b2dea062cc2ad6e121c173899e954ed6e5cfb
chakriyaP <65884346+chakriyaP@users.noreply.github.com>
2020-11-22 01:33:10 +0700

    get some

--- method/functionGet.js
+++ method/functionGet.js
@@ -20,3 +20,26 @@ export async function getAll(table) {
         });
     return res;
 }
+
+
+export async function getCustomers(req) {
+    const { fname, lname, c_id, customers_address, phone, id_line } = req.body
+    const queryString = `Select * From Where fname Like '%${fname}%' and lname Like '%${lname}%' and c_id Like '%${c_id}%' 
+    and customers_address Like '%${customers_address}%' and phone Like '%${phone}%' and id_line Like '%${id_line}%'; `;
+    let res = await connector
+        .getConnection()
+        .then(async(conn) => {
+            let response = await conn.query(queryString);
+            conn.release();
+            if (response.length != 0) {
+                return response;
+            } else {
+                return { error: response };
+            }
+        })
+        .catch((err) => {
+            conn.release();
+            return { error: err.message };
+        });
+    return res;
+}
--- routers/customers.js
+++ routers/customers.js
@@ -1,17 +1,24 @@
 import express from "express";
 import {
     getAll,
+    getCustomers
 } from "../method/functionGet.js";
 
 const route = express.Router();
 
 route.get("/", (req, res) => {
     console.log(`GET customers`);
-    getAll("customers").then((values) => {
+    getAll("customers1").then((values) => {
         console.log(values);
         res.json(values);
     });
 });
 
+route.post("/", (req, res) => {
+    getCustomers(req).then((values) => {
+        console.log(values);
+        res.json(values);
+    });
+});
 
 export default route;
