d63fe2a5dc3ef0d3dfb1450513422420b35065ee
chakriyaP <65884346+chakriyaP@users.noreply.github.com>
2020-11-21 22:05:59 +0700

    config

--- index.js
+++ index.js
@@ -3,17 +3,10 @@ import bodyParser from "body-parser";
 import path from "path";
 
 import orderRouter from "./routers/orders.js";
-<<<<<<< HEAD
 import customersRouter from "./routers/customers.js";
-=======
-<<<<<<< HEAD
 import employeeRouter from "./routers/employees.js";
-=======
 import stockRouter from "./routers/stock.js";
 import printPatternRouter from "./routers/printPattern.js";
-
->>>>>>> 0713393b3dc80b983140454d9c5b8bb989ddad1c
->>>>>>> a354691e08ba78b30b72892121cd248885e9926d
 
 const port = 3000;
 const app = express();
@@ -21,17 +14,10 @@ const _dirname = path.resolve();
 
 app.use(bodyParser.json());
 app.use("/orders", orderRouter);
-<<<<<<< HEAD
 app.use("/customers", customersRouter)
-=======
-<<<<<<< HEAD
 app.use("/employees", employeeRouter)
-=======
 app.use("/stock", stockRouter);
 app.use("/printPattern", printPatternRouter);
-
->>>>>>> 0713393b3dc80b983140454d9c5b8bb989ddad1c
->>>>>>> a354691e08ba78b30b72892121cd248885e9926d
 
 app.get("/", (req, res) => {
     res.json({ msg: "welcome delivery" });
